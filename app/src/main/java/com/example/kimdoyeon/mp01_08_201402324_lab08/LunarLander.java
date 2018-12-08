5dcb609322146988351f2507f087b4dbe20abff4
CSEKimDoYeon <ehdus0008@naver.com>
2018-12-08 18:09:15 +0900

    origin

---
+++ app/src/main/java/com/example/kimdoyeon/mp01_08_201402324_lab08/LunarLander.java
@@ -0,0 +1,100 @@
+package com.example.kimdoyeon.mp01_08_201402324_lab08;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.kimdoyeon.mp01_08_201402324_lab08.LunarView.LunarThread;
+
+
+public class LunarLander extends AppCompatActivity{
+
+    private static final int MENU_PAUSE = 4;
+
+    private static final int MENU_RESUME = 5;
+
+    private static final int MENU_START = 6;
+
+    private static final int MENU_STOP = 7;
+
+    /** A handle to the thread that's actually running the animation. */
+    private LunarThread mLunarThread;
+
+    /** A handle to the View in which the game is running. */
+    private LunarView mLunarView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(0, MENU_START, 0, R.string.menu_start);
+        menu.add(0, MENU_STOP, 0, R.string.menu_stop);
+        menu.add(0, MENU_PAUSE, 0, R.string.menu_pause);
+        menu.add(0, MENU_RESUME, 0, R.string.menu_resume);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_START:
+                mLunarThread.doStart();
+                return true;
+            case MENU_STOP:
+                mLunarThread.setState(LunarThread.STATE_LOSE,
+                        getText(R.string.message_stopped));
+                return true;
+            case MENU_PAUSE:
+                mLunarThread.pause();
+                return true;
+            case MENU_RESUME:
+                mLunarThread.unpause();
+                return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // tell system to use the layout defined in our XML file
+        setContentView(R.layout.lunar_layout);
+
+        // get handles to the LunarView from XML, and its LunarThread
+        mLunarView = (LunarView) findViewById(R.id.lunar);
+        mLunarThread = mLunarView.getThread();
+
+        // give the LunarView a handle to the TextView used for messages
+        mLunarView.setTextView((TextView) findViewById(R.id.text));
+
+        if (savedInstanceState == null) {
+            // we were just launched: set up a new game
+            mLunarThread.setState(LunarThread.STATE_READY);
+            Log.w(this.getClass().getName(), "SIS is null");
+        } else {
+            // we are being restored: resume a previous game
+            mLunarThread.restoreState(savedInstanceState);
+            Log.w(this.getClass().getName(), "SIS is nonnull");
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLunarView.getThread().pause(); // pause game when Activity pauses
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // just have the View's thread save its state into our Bundle
+        super.onSaveInstanceState(outState);
+        mLunarThread.saveState(outState);
+        Log.w(this.getClass().getName(), "SIS called");
+    }
+}
